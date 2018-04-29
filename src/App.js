f1b69267950986103fdc91848dcf48c03b6e1992
NangoliJude <fxjudemedia@gmail.com>
2018-04-29 09:43:21 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react'
+import { GrindLine } from './component/GrindLine';
+class App extends Component{  
+    render(){
+        return(
+            <main>
+                <GrindLine/>
+            </main>
+        )
+    }
+};
+
+export default App;
---
+++ src/component/GrindLine.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const GrindLine = ()=>
+<React.Fragment>
+<svg viewBox= "0,0, 100,100" style={{border: "red solid 1px"}}>
+                <path stroke="grey" style={{strokeWidth: ".2"}} d="M10 0 V100"/>
+                <path stroke="green" style={{strokeWidth: ".2"}} d="M20 0 V100"/>
+                <path stroke="pink" style={{strokeWidth: ".2"}} d="M30 0 V100"/>
+                <path stroke="orange" style={{strokeWidth: ".2"}} d="M40 0 V100"/>
+                <path stroke="yellow" style={{strokeWidth: ".2"}} d="M50 0 V100"/>
+                <path stroke="purple" style={{strokeWidth: ".2"}} d="M60 0 V100"/>
+                <path stroke="blue" style={{strokeWidth: ".2"}} d="M70 0 V100"/>
+                <path stroke="red" style={{strokeWidth: ".2"}} d="M80 0 V100"/>
+                <path stroke="black" style={{strokeWidth: ".2"}} d="M90 0 V100"/>
+
+                 <path stroke="grey" style={{strokeWidth: ".2"}} d="M0 10 H100"/>
+                 <path stroke="black" style={{strokeWidth: ".2"}} d="M0 20 H100"/>
+                 <path stroke="green" style={{strokeWidth: ".2"}} d="M0 30 H100"/>
+                 <path stroke="yellow" style={{strokeWidth: ".2"}} d="M0 40 H100"/>
+                 <path stroke="blue" style={{strokeWidth: ".2"}} d="M0 50 H100"/>
+                 <path stroke="pink" style={{strokeWidth: ".2"}} d="M0 60 H100"/>
+                 <path stroke="red" style={{strokeWidth: ".2"}} d="M0 70 H100"/>
+                 <path stroke="grey" style={{strokeWidth: ".2"}} d="M0 80 H100"/>
+                 <path stroke="green" style={{strokeWidth: ".2"}} d="M0 90 H100"/>
+                
+                </svg>
+</React.Fragment>
